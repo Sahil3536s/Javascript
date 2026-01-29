40fd77c78d9883c315bcee3ee5c91f7c54cacaaa
Sahil Sehrawat <sehrawat7253@gmail.com>
2026-01-29 16:38:00 +0530

    datatypes

---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,4 @@
+let name = "Sahil"
+let age = 19
+console.log(typeof 19)
+console.log(typeof "Sahil")
--- 01_Basics/variables.js
+++ 01_Basics/variables.js
@@ -1,4 +1,5 @@
 const accountId = 123456;
 let accountEmail = "sahil@gmail.com";
 var accountPassword = "Sahil@123";
-console.log(accountId);
+accountCity = "New Delhi"
+console.table([accountEmail, accountPassword, accountId, accountCity])
