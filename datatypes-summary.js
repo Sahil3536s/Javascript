726c7339b7c5b3dcbf764af315d91960c4c5bceb
Sahil Sehrawat <sehrawat7253@gmail.com>
2026-02-02 12:16:37 +0530

    Datatype-summary complete

---
+++ datatypes-summary.js
@@ -0,0 +1,26 @@
+// Primitive Data type
+
+// 7 types: String, number, Boolean, null, undefined, Symbol, BigInt
+// Symbol: To make any value unique
+// undefined: we have give the space but not defined the value
+// null: it is empty not 0
+// BigInt: The bigger values that are calculated by calc are comes under bigint     
+
+
+// Refrence(Non- Primitive)
+// Array, Objects and Functions
+// Array
+const game = ["COD", "Farcry", "Battlefield"]
+let obj = {
+    Name : "Sahil",
+    Age : 19
+}
+// console.log(game+ " ");
+
+// Function
+const myFunction = function(){
+    console.log("Hello");
+    
+}
+console.log(typeof myFunction);
+
