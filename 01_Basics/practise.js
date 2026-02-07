f7430e068389f0c75604a796c311168f47521737
Sahil Sehrawat <sehrawat7253@gmail.com>
2026-02-07 21:20:34 +0530

    practise today

--- 01_Basics/practise.js
+++ 01_Basics/practise.js
@@ -1,7 +1,27 @@
-const myName = "Sahil"
-let number = 48.3
-console.log(Math.floor(4.3));
+const score = 400
+// console.log(score);
 
-console.log(Math.random()*10 + 1);
+const balance = new Number(100) // it specifies tells this is a number
+// console.log(balance);
+// console.log(balance.toString().length);  // used to convert into string
+// console.log(balance.toFixed(2)); // Used to give two zero digit after decimal
+
+// ++++++++++++++  Maths  +++++++++++++++++++++++++++
+console.log(Math.abs(-99));  // Convert a negative number to postive
+console.log(Math.round(99.6)); // Gives a roundoff value
+console.log(Math.min(5,4,8,3)); // Gives the minimum value
+console.log(Math.max(4,9,8,3)); // Gives the maximum value
+console.log(Math.random()); // Gives the randomm value between 0 and 1
+console.log(Math.random()*10); // Now the value start from 1 but if the random value is 0.02 then it gives the value in 0 to solve this see next line code
+console.log(Math.random()*10 + 1); // It will never give the value in 0
+console.log(Math.floor(4.2)); // Gives the minimum roundoff value
+console.log(Math.floor(Math.random()*10) + 1); // GIves the value not in decimal
+
+
+
+
+
+
+
 
 
