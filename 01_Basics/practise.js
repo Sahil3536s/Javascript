a65854968b60d9bfb453108afeb6b64c62586daf
Sahil Sehrawat <sehrawat7253@gmail.com>
2026-02-09 21:02:16 +0530

    Tomorrow Paper

--- 01_Basics/practise.js
+++ 01_Basics/practise.js
@@ -1,27 +1,31 @@
-const score = 400
-// console.log(score);
+let myDate = new Date()
+// console.log(myDate);
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toTimeString());
+// console.log(typeof myDate);
 
-const balance = new Number(100) // it specifies tells this is a number
-// console.log(balance);
-// console.log(balance.toString().length);  // used to convert into string
-// console.log(balance.toFixed(2)); // Used to give two zero digit after decimal
+// let myCreateddate = new Date(2026, 2, 3)  // In js months start from zero
+let myCreateddate = new Date("2026-03-03")
+// console.log(myCreateddate.toLocaleString());
+// let myTimeddate = new Date(2026, 2, 3, 12, 0) 
+// console.log(myTimeddate.toLocaleString());
 
-// ++++++++++++++  Maths  +++++++++++++++++++++++++++
-console.log(Math.abs(-99));  // Convert a negative number to postive
-console.log(Math.round(99.6)); // Gives a roundoff value
-console.log(Math.min(5,4,8,3)); // Gives the minimum value
-console.log(Math.max(4,9,8,3)); // Gives the maximum value
-console.log(Math.random()); // Gives the randomm value between 0 and 1
-console.log(Math.random()*10); // Now the value start from 1 but if the random value is 0.02 then it gives the value in 0 to solve this see next line code
-console.log(Math.random()*10 + 1); // It will never give the value in 0
-console.log(Math.floor(4.2)); // Gives the minimum roundoff value
-console.log(Math.floor(Math.random()*10) + 1); // GIves the value not in decimal
+let myTimestamp = Date.now()
+// console.log(myTimestamp);  // It gives us millisecond value
 
+console.log(Math.floor(Date.now()/1000));
 
-
+let newDate = new Date()
+console.log(newDate.getDay());
+console.log(newDate.getFullYear());
+ console.log(newDate.getMonth());
+ console.log(newDate.getDate());
 
 
-
-
-
+ newDate.toLocaleString('deafult', {
+    weekday : "long"
+ })
+ 
 
