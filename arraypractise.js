65b5494e672ad4eb2df901ef9456d57e0d300851
Sahil Sehrawat <sehrawat7253@gmail.com>
2026-02-14 21:50:10 +0530

    timepass

---
+++ arraypractise.js
@@ -0,0 +1,16 @@
+const my_sons = ["Vishal", "Nanda", "Kalu" ]
+const new_sons = ["Harish", "Chinna"]
+
+my_sons.push(new_sons)  // Combines two array 
+console.log(my_sons);
+
+// const all_sons = my_sons.concat(new_sons) // Combines two array and return a new array
+const all_sons = [...my_sons, ...new_sons]
+console.log(all_sons);
+
+const another_array = [1,3,3, [4,5], 6,7, [6,8,9]]
+console.log(another_array.flat(Infinity));
+
+console.log(Array.from("Sahil"));
+console.log(5+3);
+
