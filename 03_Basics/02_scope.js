bda9c0c660c22853eb454ad999ab51afae52dd2b
Sahil Sehrawat <sehrawat7253@gmail.com>
2026-02-22 11:47:12 +0530

    Scope with different function syntax done

--- 03_Basics/02_scope.js
+++ 03_Basics/02_scope.js
@@ -19,3 +19,43 @@ printName2(); // Output: Sahil
 // console.log(name2); // Error: name2 is not defined
 
 
+function one(){
+    const username = "Sahil"
+    function two(){
+        const website = "Youtube"
+        console.log(username); // Output: Sahil  it is the global scope and can be accessed anywhere
+    }
+    // console.log(website); // It is the local scope cannot accessed outsite the function 
+
+    two()
+    console.log(username);
+    
+    
+}
+one()
+
+if(true){
+   const username = "Sahil"
+    if(username === "Sahil"){
+        const website = " Youtube"
+        console.log(username + website);
+    }
+    // console.log(website); // Error
+}
+// console.log(username ); // Error
+    
+// ++++++++++++++++++++ Interesting ++++++++++++++++++++
+console.log(addOne(5));  // It not give error we do it with normal function syntax
+function addOne(num){
+    return num + 1
+}
+
+// console.log(addTwo(5));  // It gives error because after declare it we hold it in a variable  addTwo
+const addTwo = function(num){
+    return num + 2
+}
+console.log(addTwo(5));
+
+
+
+      
